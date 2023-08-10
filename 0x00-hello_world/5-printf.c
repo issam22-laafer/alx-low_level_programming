0a5cd256adaf3d6e6a5c17fc5ba5aadabfbbeec6
ISSAM LAAFAR <issamlaafer509@gmail.com>
2023-08-10 15:32:56 -0700

    m

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,1 +1,12 @@
-#!/bin/bash
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ *Description: A C program that prints with printf  function
+ *Return: Alawys 0 (Success)
+*/
+int main(void)
+{
+        printf("with proper grammar, but the outcome is a piece of art,");
+        return (0);
+} 
